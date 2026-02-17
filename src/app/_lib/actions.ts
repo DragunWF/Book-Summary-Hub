dd0ea9c72a9df7eb91051670c91554cb4528d78d
DragunWF <plarisanmarc2@gmail.com>
2026-02-17 14:05:46 +0800

    feat: create placeholder functions on server actions

--- src/app/_lib/actions.ts
+++ src/app/_lib/actions.ts
@@ -1,3 +1,43 @@
+// Book Summaries
+
 export async function createBookSummary() {
+  return;
+}
+
+export async function updateBookSummary() {
+  return;
+}
+
+export async function deleteBookSummary() {
+  return;
+}
+
+export async function archiveBookSummary() {
+  return;
+}
+
+export async function getBookSummaries() {
+  return;
+}
+
+export async function getBookSummary(id: number) {
+  return;
+}
+
+export async function getFeaturedBookSummary(id: number) {
+  return;
+}
+
+// Comments
+
+export async function getComments(bookId: number) {
+  return;
+}
+
+export async function postComment(bookId: number, comment: string) {
+  return;
+}
+
+export async function deleteComment() {
   return;
 }
