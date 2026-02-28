eef88ceed1052837120ab6815e4508e43eb529f6
DragunWF <plarisanmarc2@gmail.com>
2026-02-28 11:07:16 +0800

    fix(supabase): close typing warnings from supabase client config

--- src/app/_lib/supabase.ts
+++ src/app/_lib/supabase.ts
@@ -1,7 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
-// import type { Database } from "@/database.types";
 
 export const supabase = createClient(
-  process.env.SUPABASE_URL,
-  process.env.SUPABASE_KEY,
+  process.env.SUPABASE_URL!,
+  process.env.SUPABASE_KEY!,
 );
